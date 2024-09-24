ce18190f6b1e8fb9f0e3af8a21f767e2dca5a7cf
rachanarajurwar <rachana.rajurwar@hwyhaul.com>
2024-09-24 12:38:10 -0700

    My version

--- src/main/java/io/javabrains/inbox/email/EmailRepository.java
+++
@@ -1,9 +0,0 @@
-package io.javabrains.inbox.email;
-
-import java.util.UUID;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-
-public interface EmailRepository extends CassandraRepository<Email, UUID>  {
-
-}
